9193d6bda4cb34e2896dca4e9b077d207ecb3bdb
Stephen M. Gary <stgary0@gmail.com>
2020-05-22 16:14:55 -0400

    more cypress changes

--- cypress/integration/form_tests.spec.js
+++ cypress/integration/form_tests.spec.js
@@ -7,13 +7,21 @@ describe('Form inputs', () => {
       cy.get('button.order')
         .click()
     })
-  
 
     it('can type a name', () => {
       cy.get('input[name="name"]')
         .type('Stephen Gary')
         .should('have.value', 'Stephen Gary')
     })
+
+    it('can select a size', () => {
+      cy.get('select[name="size"]').select('Large')
+    })
+
+    it('can select a crust', () => {
+      cy.get('input[name="crust"]').check()
+    })
+
 
     it('can check toppings', () => {
       cy.get('input[name="pepperoni"]').check()
@@ -22,14 +30,11 @@ describe('Form inputs', () => {
       cy.get('input[name="pineapple"]').check()
     })
 
-  })
-
-  describe('Submitting', () => {
-    it('can submit', () => {
+    it('can comment', () => {
       cy.get('input[name="comments"]').type('Please put extra cheese on my pizza')
-      cy.get('select[name="size"]').select('Large')
-      cy.get('input[name="sausage"]').check()
-      cy.get('input[name="pineapple"]').check()
+    })
+
+    it('can submit', () => {
       cy.get('button.submit').click()
       cy.get('.order h2').contains('Stephen Gary')
     })
